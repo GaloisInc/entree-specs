f48644ad1e8d1a1d7247ec388cf39df90a926274
Lucas Silver <lucsil@seas.upenn.edu>
2022-09-15 09:41:22 -0400

    have fully typechecking, axiom-free draft of denotation

--- theories/AlmostStlc/Denotation.v
+++ theories/AlmostStlc/Denotation.v
@@ -1,44 +1,16 @@
+Require Export MrecMonad.
+Require Export HeterogeneousRelations.
+Require Export EnTreeDefinition.
+Require Export Syntax.
+From Coq Require Import Lists.List.
+
+From Equations Require Import Equations Signature.
+
+Import Monads.
+Import MonadNotation.
+Local Open Scope monad_scope.
 
 
-Inductive var {T : Type} : T -> list T -> Type := 
-  | VarZ (x : T) l : var x (x :: l)
-  | VarS (x y : T) l : var x l -> var x (y :: l).
-
-
-Inductive type : Type :=
-  | Nat : type
-  | List : type -> type
-                   (* should really be a nonempty list *)
-  | Arrow : type -> list (type * type) -> type -> type
-.
-Notation ctx := (list type).
-Notation rec_ctx := (list (type * type))%type.
-
-
-
-(*TODO : add some basic natural number operations *)
-Inductive term : type -> ctx -> rec_ctx -> Type := 
-  | term_const (n : nat) Γ R : term Nat Γ R
-  | term_nil t Γ R : term (List t) Γ R
-  | term_cons t Γ R (head : term t Γ R) (tail : term (List t) Γ R) : term (List t) Γ R
-  | term_var t Γ R (x : var t Γ) : term t Γ R
-
-  | term_match_nat t Γ R (en : term Nat Γ R) (eZ : term t Γ R) (eS : term t (Nat :: Γ) R ) : term t Γ R
-  | term_match_list t1 t2 Γ R (el : term (List t1) Γ R ) (enil : term t2 Γ R) (econs : term t2 (t1 :: Γ) R) : term t2 Γ R
-  (* might be some subtleties in the application rule you are not sufficiently considering *)
-  | term_app t1 t2 Γ R (e1 : term (Arrow t1 R t2) Γ R) (e2 : term t1 Γ R) : term t2 Γ R
-  (* same here *)
-  | term_abs t1 t2 Γ R R' (e : term t2 (t1 :: Γ) R') : term (Arrow t1 R' t2) Γ R
-  | term_call t1 t2 Γ R (x : var (t1,t2) R) (arg : term t1 Γ R) : term t2 Γ R
-  (* this is the most novel part so be very careful *)
-  | term_mfix t Γ R R' (bodies : mfix_bodies Γ R R) (e : term t Γ R) : term t Γ R'
-
-
-with mfix_bodies : ctx -> rec_ctx -> rec_ctx -> Type := 
-  | mfix_bodies_nil Γ R : mfix_bodies Γ R nil
-  | mfix_bodies_cons Γ R t1 t2 R' (body : term t2 (t1 :: Γ) R) (bodies : mfix_bodies Γ R R') : mfix_bodies Γ R ((t1,t2) :: R')
-.
-(*
 Fixpoint denote_rec_ctx' (R: list (Type * Type) ) : Type@{entree_u} :=
   match R with
   | nil => void
@@ -180,17 +152,3 @@ where denote_bodies (Γ : ctx) (R R' : rec_ctx) (bodies : mfix_bodies Γ R R') (
     denote_bodies Γ' R1 R2 bodies' hyps' arg'.
 Print Assumptions denote_term. (* closed under global context *)
 
-(*one more potential roadblock (might just require equations) need to statically
-  understand the return type of a call *)
-
-(* Fixpoint subst_term t1 t2 Γ R (e : ) *)
-
-
-(*
-TODO: substitution, value, small step semantics
-      mrec monad, denotational semantics
-
-      prove give decode_inject a sane definition (focus on removing the JMeq axiom, which is coming from the usage of dependent induction), move to denotational sep file, find way to test for correctness + 
-*)
-
-*)
--- theories/AlmostStlc/MrecMonad.v
+++ theories/AlmostStlc/MrecMonad.v
@@ -25,43 +25,35 @@ Require Import Program.Tactics.
 
 Require Import ITree.Basics.Basics.
 
+Variant void : Set := .
+Global Instance voidEncodedType : EncodedType void := fun v => match v with end.
+
 
 (* could extend to have arbitrary constraints on E, could allow E to include other events*)
 (* universe inconsistenty traces back to here, for now could specialize E to void, perhaps,
    this whole mtree idea was a holdover from when you had more complicated mfix effects *)
 Definition mtree (D : Type) `{EncodedType D} (A : Type) : Type@{entree_u}:= 
-  forall E `(EncodedType E), entree (D + E) A.
+   entree (D + void) A.
 
-Global Instance mtree_monad D `{EncodedType D} : Monad (mtree D) :=
- {|
-   ret := fun _ a E encodes => ret a;
-   bind := fun _ _ m k E encodes => EnTree.bind (m E encodes) (fun a => k a E encodes)
- |}.
+Global Instance mtree_monad D `{EncodedType D} : Monad (mtree D) := Monad_entree.
 
 
-Definition call {D} `{EncodedType D} (d : D) : mtree D (encodes d) :=
-  fun _ _ => trigger d.
+Definition call {D} `{EncodedType D} (d : D) : mtree D (encodes d) := trigger d.
 
-CoFixpoint interp_mtree' {D D' E R} `{EncodedType D} `{EncodedType D'} `{EncodedType E}
-           (bodies : forall (d : D), entree (D + E) (encodes d)) (ot : entree' (D + E) R ) : 
-  entree (D' + E) R :=
+CoFixpoint interp_mtree' {D D' R} `{EncodedType D} `{EncodedType D'}
+           (bodies : forall (d : D), entree (D + void) (encodes d)) (ot : entree' (D + void) R ) : 
+  entree (D' + void) R :=
   match ot with
   | RetF r => Ret r
   | TauF t => Tau (interp_mtree' bodies (observe t))
   | VisF (inl d) k => 
       Tau (interp_mtree' bodies (observe (EnTree.bind (bodies d) k ) ) )
-  | VisF (inr e) k => Vis (@inr D' E e) (fun x => interp_mtree' bodies (observe (k x)))
+  | VisF (inr v) k => Vis (@inr D' void v) (fun x => interp_mtree' bodies (observe (k x)))
   end.
 
-Definition interp_mtree {D D' E R} `{EncodedType D} `{EncodedType D'} `{EncodedType E}
-           (bodies : forall (d : D), entree (D + E) (encodes d)) (t : entree (D + E) R) :
-  entree (D' + E) R := interp_mtree' bodies (observe t).
-
-
-Definition resolve {D D' R} `{EncodedType D} `{EncodedType D'} 
-           (bodies : forall (d : D), mtree D (encodes d)) (m : mtree D R) : mtree D' R :=
-fun E encodes => interp_mtree (fun d => bodies d E encodes) (m E encodes).
-
+Definition interp_mtree {D D' R} `{EncodedType D} `{EncodedType D'}
+           (bodies : forall (d : D), entree (D + void) (encodes d)) (t : entree (D + void) R) :
+  entree (D' + void) R := interp_mtree' bodies (observe t).
 
 
 (* define monad structure*)
