1036455267df910fce80356600910b5601634415
Eddy Westbrook <westbrook@galois.com>
2023-02-20 07:01:44 -0800

    defined a version of LetRecType and the accompanying versions of LRTInput, LRTOutput, FrameCall, and FrameCallRet to hopefully support higher-order recursive functions

--- theories/Ref/SpecM.v
+++ theories/Ref/SpecM.v
@@ -26,6 +26,154 @@ Local Open Scope list_scope.
 
 Import Monads.
 
+
+(** A new approach to LetRecType **)
+
+(* A version of nth_default that does primary recursion on the list *)
+Fixpoint nth_default' {A} (d : A) (l : list A) n : A :=
+  match l with
+  | nil => d
+  | x :: l' => match n with
+               | 0 => x
+               | S n' => nth_default' d l' n'
+               end
+  end.
+
+(* An encoded argument type for a recursive function *)
+Inductive LRTArgType : Type@{entree_u + 1} :=
+| ArgType_Const (A : Type@{entree_u}) : LRTArgType
+| ArgType_Prod (A B : LRTArgType) : LRTArgType
+| ArgType_Sigma (A : Type@{entree_u}) (B : A -> LRTArgType) : LRTArgType
+| ArgType_Arrow (A B : LRTArgType) : LRTArgType
+| ArgType_Pi (A : Type@{entree_u}) (B : A -> LRTArgType) : LRTArgType
+| ArgType_M (A : LRTArgType) : LRTArgType
+.
+
+(* An element of an LRTArgType, which is either a decoding of the LRTArgType to
+an actual Coq type or a recursive call of the given type in the frame *)
+Fixpoint LRTArgTypeElem (M : Type@{entree_u} -> Type@{entree_u})
+  (argTp : LRTArgType) : Type@{entree_u} :=
+  match argTp with
+  | ArgType_Const A => A
+  | ArgType_Prod A B => LRTArgTypeElem M A * LRTArgTypeElem M B
+  | ArgType_Sigma A B => { x:A & LRTArgTypeElem M (B x) }
+  | ArgType_Arrow A B => LRTArgTypeElem M A -> LRTArgTypeElem M B
+  | ArgType_Pi A B => forall x:A, LRTArgTypeElem M (B x)
+  | ArgType_M A => M (LRTArgTypeElem M A)
+  end.
+
+(* The type of a recursive function *)
+Inductive LetRecType : Type@{entree_u + 1} :=
+| LRT_Ret (R : LRTArgType) : LetRecType
+| LRT_FunDep (A : Type@{entree_u}) (rest : A -> LetRecType) : LetRecType
+| LRT_Fun (A : LRTArgType) (rest : LetRecType) : LetRecType
+.
+
+(* A recursive function frame is specified by a list of LetRecTypes *)
+Definition RecFrame := list LetRecType.
+
+(* Get the nth element of a RecFrame list, or void -> void if n is too big *)
+Definition nthLRT (frame : RecFrame) n : LetRecType :=
+  nth_default' (LRT_Fun (ArgType_Const void) (LRT_Ret (ArgType_Const void))) frame n.
+
+(* Compute a dependent tuple type of all the inputs of a LetRecType, i.e.,
+   return the type { x:A & { y:B & ... { z:C & unit } ...}} from a LetRecType
+   that represents forall a b c..., SpecM ... (R a b c ...). The supplied type
+   function M gives the interpretation of the ArgType_M type constructor. *)
+Fixpoint LRTInput (M : Type@{entree_u} -> Type@{entree_u}) lrt : Type@{entree_u} :=
+  match lrt with
+  | LRT_Ret _ => unit
+  | LRT_FunDep A rest => {a : A & LRTInput M (rest a) }
+  | LRT_Fun A rest => LRTArgTypeElem M A * LRTInput M rest
+  end.
+
+(* Compute the output type (R a b c ...) from a LetRecType that represents
+   forall a b c..., SpecM ... (R a b c ...) and a dependent tuple of arguments
+   to a function of that type, using M for the ArgType_M type constructor *)
+Fixpoint LRTOutput M lrt : EncodingType (LRTInput M lrt) :=
+  match lrt with
+  | LRT_Ret R => fun _ => LRTArgTypeElem M R
+  | LRT_FunDep A rest => fun args =>
+                           let '(existT _ a args') := args in
+                           LRTOutput M (rest a) args'
+  | LRT_Fun A rest => fun args => LRTOutput M rest (snd args)
+  end.
+
+(* A recursive call to one of the functions in a RecFrame, specified by a
+natural number n that picks a specific index in the RecFrame along with a set of
+arguments for that recursive call, using the given type function M as the
+interpretation of the ArgType_M constructor *)
+Inductive FrameCallH M frame : Type@{entree_u} :=
+| FrameCallOfArgs n (args : LRTInput M (nthLRT frame n)).
+
+(* The return type for a FrameCallH recursive call, using a specific M *)
+Definition FrameCallHRet M frame (args: FrameCallH M frame) : Type@{entree_u} :=
+  match args with
+  | FrameCallOfArgs _ _ n args => LRTOutput M (nthLRT frame n) args
+  end.
+
+(* The type of a FrameCallH recursive call along with a FrameCallHRet function
+that computes the return type of one of those recursive calls, but where the
+arguments to the recursive call are at a specified "level". A level 0 frame call
+is one whose higher-order arguments can only contain calls to monadic functions
+of return type M0, while a level S lvl' call can contain higher-order arguments
+with frame calls at level lvl' or below. *)
+Fixpoint FrameCallLvlInOut (M0 : Type@{entree_u} -> Type@{entree_u}) frame lvl
+  : { T:Type@{entree_u} & T -> Type@{entree_u} } :=
+  match lvl with
+  | 0 => existT (fun T : Type@{entree_u} => T -> Type@{entree_u}) (FrameCallH M0 frame) (FrameCallHRet M0 frame)
+  | S lvl' =>
+      let M := fun R =>
+                 (M0 R +
+                    { args:projT1 (FrameCallLvlInOut M0 frame lvl')
+                    | projT2 (FrameCallLvlInOut M0 frame lvl') args = R })%type in
+      existT _ (FrameCallH M frame) (FrameCallHRet M frame)
+  end.
+
+(* A frame call at an arbitrary level, where M0 defines level 0 *)
+Definition FrameCall (M0 : Type@{entree_u} -> Type@{entree_u}) frame :=
+  { lvl & projT1 (FrameCallLvlInOut M0 frame lvl) }.
+
+(* Compute the return type of a FrameCall *)
+Definition FrameCallRet (M0 : Type@{entree_u} -> Type@{entree_u}) frame
+                        (call : FrameCall M0 frame) : Type@{entree_u} :=
+  projT2 (FrameCallLvlInOut M0 frame (projT1 call)) (projT2 call).
+
+(* Examples of higher-order functions we want to write (but without monadic types) *)
+Section HOExamples.
+
+Context M `{Monad M}.
+
+(* An inconsistent fix, used to write our examples with the least fuss *)
+Variable fixM : forall {A B}, ((A -> M B) -> A -> M B) -> A -> M B.
+Variable fixM2 : forall {A B C}, ((A -> B -> M C) -> A -> B -> M C) -> A -> B -> M C.
+
+Definition incrStream (str : nat -> M nat) n : M nat :=
+  bind (str n) (fun x => ret (x + 1)).
+
+Definition foo : (nat -> M nat) -> nat -> M nat :=
+  fixM2 (fun rec str n =>
+          match n with
+          | 0 => str 0
+          | S n' => rec (rec (incrStream str)) n'
+          end).
+
+Definition fibMap : (nat -> M nat) -> nat -> M nat :=
+  fixM2 (fun rec str n =>
+           match n with
+           | 0 => str 0
+           | S 0 => bind (rec str 0) (fun x => ret (x + 1))
+           | S (S n') =>
+               bind (rec str n')
+                 (fun x =>
+                    bind (rec str (S n'))
+                         (fun y => ret (x + y)))
+           end).
+
+End HOExamples.
+
+FIXME: update the following
+
 (* An encoding of types of form forall a b c..., SpecM ... (R a b c ...). This
    encoding is realized by LRTType below. *)
 Inductive LetRecType : Type@{entree_u + 1} :=
@@ -347,7 +495,7 @@ Definition CallS E Γ frame (args : FrameCall frame) :
   trigger (H2:=@SpecEventReSumRet _ _ _ _ _ _) args.
 
 (* Build the right-nested tuple type of a list of functions in a RecFrame *)
-Fixpoint FrameTuple E Γ (frame : RecFrame) : Type :=
+Fixpoint FrameTuple E Γ (frame : RecFrame) : Type@{entree_u} :=
   match frame with
   | nil => unit
   | lrt :: frame' => LRTType E Γ lrt * FrameTuple E Γ frame'
@@ -380,6 +528,85 @@ Definition MultiFixS E Γ frame
            (bodies : FrameTuple E (frame :: Γ) frame)
            (call : FrameCall frame) : SpecM E Γ (FrameCallRet frame call) :=
   mrec_spec (applyFrameTuple E (frame :: Γ) frame bodies) call.
+
+
+
+(** Stack functors and using them in MultiFixS **)
+
+(* A StackType is a type that depends on a FunStack *)
+Definition StackType := FunStack -> Type@{entree_u}.
+
+(* A stack functor is a type which depends on the current FunStack that supports
+pushing and popping the FunStack as long as we have an interpretation of each
+frame that is pushed or popped *)
+Class IsStackFunc E (T : StackType) :=
+  { pushSF : forall Γ frame,
+      FrameTuple E (frame :: Γ) frame ->
+      T Γ -> T (frame :: Γ);
+    popSF : forall Γ frame,
+      FrameTuple E (frame :: Γ) frame ->
+      T (frame :: Γ) -> T Γ; }.
+
+Definition constST T : StackType := fun _ => T.
+
+#[global] Instance IsStackFunc_const E T : IsStackFunc E (constST T) :=
+  { pushSF := fun _ _ _ x => x; popSF := fun _ _ _ x => x }.
+
+Definition prodST (T1 T2 : StackType) : StackType :=
+  (fun stk => prod (T1 stk) (T2 stk)).
+
+#[global] Instance IsStackFunc_pair E T1 T2
+  {isSF1 : IsStackFunc E T1} {isSF2 : IsStackFunc E T2} :
+  IsStackFunc E (prodST T1 T2) :=
+  { pushSF := fun stk frame interp tup => (pushSF stk frame interp (fst tup),
+                                            pushSF stk frame interp (snd tup));
+    popSF := fun stk frame interp tup => (popSF stk frame interp (fst tup),
+                                           popSF stk frame interp (snd tup)); }.
+
+Definition arrowST (T U : StackType) : StackType :=
+  fun stk => T stk -> U stk.
+
+#[global] Instance IsStackFunc_fun E T1 T2 
+  {isSF1 : IsStackFunc E T1} {isSF2 : IsStackFunc E T2} :
+  IsStackFunc E (arrowST T1 T2) :=
+  { pushSF := fun stk frame interp f x =>
+                pushSF stk frame interp (f (popSF stk frame interp x));
+    popSF := fun stk frame interp f x =>
+               popSF stk frame interp (f (pushSF stk frame interp x)); }.
+
+(* A variant of LetRecType where arguments and the return type can depend on the
+current stack, though arguments that do are not allowed to be type indices *)
+Inductive LetRecTypeSF E : Type@{entree_u + 1} :=
+| LRTSF_Ret R `{IsStackFunc E R} : LetRecTypeSF E
+| LRTSF_Fun (A : Type@{entree_u}) (rest : A -> LetRecTypeSF E) : LetRecTypeSF E
+| LRTSF_FunSF (A : StackType) `{IsStackFunc E A} (rest : LetRecTypeSF E) : LetRecTypeSF E.
+
+(* Convert a LetRecTypeSF to a LetRecType by applying all its stack functors *)
+Fixpoint applyLetRecTypeSF E stack (lrtSF : LetRecTypeSF E) : LetRecType :=
+  match lrtSF with
+  | LRTSF_Ret _ R => LRT_Ret (R stack)
+  | LRTSF_Fun _ A rest => LRT_Fun A (fun x => applyLetRecTypeSF E stack (rest x))
+  | LRTSF_FunSF _ A rest => LRT_Fun (A stack) (fun _ => applyLetRecTypeSF E stack rest)
+  end.
+
+(* A RecFrame of stack functor LetRecTypes *)
+Definition RecFrameSF E := list (LetRecTypeSF E).
+
+(* Convert a RecFrameSF to a RecFrame by applying all its LetRecTypeSFs *)
+Fixpoint applyRecFrameSF E stack (frame : RecFrameSF E) : RecFrame :=
+  match frame with
+  | nil => nil
+  | lrtSF :: frame' =>
+      applyLetRecTypeSF E stack lrtSF :: applyRecFrameSF E stack frame'
+  end.
+
+(* Create a multi-way fixed point of a sequence of functions *)
+Definition MultiFixS_SF E Γ frame
+           (bodies : FrameTuple E (frame :: Γ) frame)
+           (call : FrameCall frame) : SpecM E Γ (FrameCallRet frame call) :=
+  mrec_spec (applyFrameTuple E (frame :: Γ) frame bodies) call.
+
+
 
 
 (** Notations in terms of the SpecM combinators **)
